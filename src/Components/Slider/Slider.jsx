d4ed35f5ec6d5b68dce273b150ae74594cbdf774
rasamkamyar <rasamkamyar@gamil.com>
2024-01-18 11:39:00 +0330

    LAST EDIT

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,24 +1,21 @@
+
 import React from "react";
 import "./Slider.css"
-// import IMAGES from "../images" // moshkele address dehi b proje
-
+// import IMAGES from "../images"   // moshkele address dehi b proje
 
 
 export default function Slider() {
-    const IMAGES = ["banner.jpg", "book banner 1.jpg"]
 
-    const [imageData, setImageData] = React.useState(["banner.jpg"])
-    console.log(imageData)
-
+    const [imageData, setImageData] = React.useState(["HARCHI"])  //VORUDI STATE TASIRI RU AMALKARD NADARE 
 
     function changeSlider() {
 
         setImageData(prevImage => !prevImage)
+        // console.log(prevImage) // not defined
+
     }
 
-
-
     return (<div className="banner" onClick={changeSlider}>
-        <img src={`../src/images/${imageData ? "banner.jpg" : "book banner 1.jpg"}`} className="banner--img" />
+        <img src={`./src/images/${imageData ? "banner4.jpg" : "banner3.jpg"}`} className="banner--img" />
     </div>)
 }
